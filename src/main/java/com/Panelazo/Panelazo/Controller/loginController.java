2aead2e7dff2b5a2e95865b26ad3ce91fb85cda3
root <jorgesandoval529@gmail.com>
2022-03-08 16:01:25 -0500

    login cross

--- src/main/java/com/Panelazo/Panelazo/Controller/loginController.java
+++ src/main/java/com/Panelazo/Panelazo/Controller/loginController.java
@@ -2,15 +2,18 @@ package com.Panelazo.Panelazo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Panelazo.Panelazo.Dao.usuarioDao;
 import com.Panelazo.Panelazo.Modelo.usuario;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class loginController {
 	@Autowired
 	private usuarioDao usuariodao;
